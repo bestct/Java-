a7cdf6aebc43d61e5f0a8b766764969466b2a7bc
bestct <59239641+bestct@users.noreply.github.com>
2020-06-27 09:42:26 +0800

    Update Helloworld1.java

--- Helloworld1.java
+++ Helloworld1.java
@@ -3,8 +3,8 @@ public class Helloworld1{
     public static void main(String[] args)
     {
         System.out.println(args[0]);
-        System.out.println(args[1]);
-
+        System.out.println（args [1]）;
+//在控制台输入hello world之后显示hello world;
 
     }
 
